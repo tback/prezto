0467e882174bc91aeb0d3f91949b1e253b41e5bd
Till Backhaus <till@backha.us>
2014-04-01 08:08:34 +0200

    enable virtualenv prompt

--- modules/python/init.zsh
+++ modules/python/init.zsh
@@ -40,7 +40,7 @@ if (( $+commands[virtualenvwrapper.sh] )); then
   export WORKON_HOME="$HOME/.virtualenvs"
 
   # Disable the virtualenv prompt.
-  VIRTUAL_ENV_DISABLE_PROMPT=1
+  VIRTUAL_ENV_DISABLE_PROMPT=0
 
   source "$commands[virtualenvwrapper.sh]"
 fi
